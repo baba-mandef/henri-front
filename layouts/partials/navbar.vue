f99004bdd6254e2bcfd9ab64407807c539572f88
henri-dev <pariso03henri@gmail.com>
2021-12-03 08:13:16 +0100

    - add navbar and footer partial components

---
+++ layouts/partials/navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <b-navbar class="is-spaced" wrapper-class="container">
+    <template #brand>
+      <b-navbar-item tag="router-link" :to="{ path: '/' }">
+        <img
+          src="./../../assets/logo.png"
+          alt="Imagination beyond compréhension"
+        />
+      </b-navbar-item>
+    </template>
+    <template #end>
+      <b-navbar-item>
+        <nuxt-link to="/">Accueil</nuxt-link>
+      </b-navbar-item>
+      <b-navbar-item>
+        <nuxt-link to="/blog">Blog</nuxt-link>
+      </b-navbar-item>
+      <b-navbar-item>
+        <nuxt-link to="/work">Projets</nuxt-link>
+      </b-navbar-item>
+      <b-navbar-item>
+        <nuxt-link to="/service">Services</nuxt-link>
+      </b-navbar-item>
+    </template>
+
+    <!-- <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Sign up</strong>
+                    </a>
+                    <a class="button is-light">
+                        Log in
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template> -->
+  </b-navbar>
+</template>
+
+<style>
+.navbar a {
+  font-weight: bold;
+  color: white;
+}
+a.nuxt-link-exact-active {
+  color: lightblue;
+}
+</style>
