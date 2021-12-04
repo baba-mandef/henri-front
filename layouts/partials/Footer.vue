cb4d55180f832befafcc35dd80ab56c41f33fdec
henri-dev <pariso03henri@gmail.com>
2021-12-04 10:34:12 +0100

    - Update Footer

--- layouts/partials/Footer.vue
+++ layouts/partials/Footer.vue
@@ -1,20 +1,22 @@
 <template>
   <footer class="footer">
     <div class="columns">
-        <div class="column is-4">
+        <div class="column">
             <div class="content has-text-centered">
       <p>
         <strong>Henri-dev </strong> par
-        <a href="https://jgthms.com">Sadi Henri Paraïso</a>. <br>
+        <a href="https://henri-dev.com">Sadi Henri Paraïso</a>. <br>
          Développé avec   <a href="https://nuxtjs.org">Nuxtjs </a> +  <a href="https://buefy.org">Buefy </a> + <a href="https://www.djangoproject.com/m">Django</a>. <br>
-         Code souce déposé sous la licence
+        <a href="https://github.com/shadowcompiler/henri-front">Code source </a> déposé sous la licence
         <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. <br>
         Le contenu de ce site est déposé sous la licence
         <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
           >CC BY NC SA 4.0</a
-        >.
+        >
       </p>
         </div>
+    </div>
+   
     </div>
     </div>
   </footer>
