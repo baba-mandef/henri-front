56196921669d4cea77218e379c18736cbcad86d9
henri-dev <pariso03henri@gmail.com>
2021-12-13 13:50:29 +0100

    - blog home

---
+++ pages/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+    
+<section class="section">
+<BlogCardList>
+    
+</BlogCardList>
+</section>
+
+</template>
+
+<script>
+
+import BlogCardList from '../../components/BlogCardList.vue'
+export default {
+    layout: 'blog',
+    components: {BlogCardList,}
+}
+</script>
