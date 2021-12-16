8a8e8e7349b5aa11f3fdd5e9c0be6a299b456dcb
henri-dev <pariso03henri@gmail.com>
2021-12-16 10:12:24 +0100

    - update footer

--- layouts/partials/Footer.vue
+++ layouts/partials/Footer.vue
@@ -1,21 +1,21 @@
 <template>
   <footer class="footer">
-  
-            <div class="content has-text-centered">
+    <div class="content has-text-centered">
       <p>
         <strong>Henri-dev </strong> par
-        <a href="https://henri-dev.com">Sadi Henri Paraïso</a>. <br>
-         Développé avec   <a href="https://nuxtjs.org">Nuxtjs </a> +  <a href="https://buefy.org">Buefy </a> + <a href="https://www.djangoproject.com/m">Django</a>. <br>
-        <a href="https://github.com/shadowcompiler/henri-front">Code source </a> déposé sous la licence
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. <br>
+        <a href="https://henri-dev.com">Sadi Henri Paraïso</a>. <br />
+        Développé avec <a href="https://nuxtjs.org">Nuxtjs </a> +
+        <a href="https://buefy.org">Buefy </a> +
+        <a href="https://www.djangoproject.com/m">Django</a>. <br />
+        <a href="https://github.com/shadowcompiler/henri-front">Code source </a>
+        déposé sous la licence
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. <br />
         Le contenu de ce site est déposé sous la licence
         <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
           >CC BY NC SA 4.0</a
         >
+        <br />
       </p>
-  
-   
-    </div>
     </div>
   </footer>
 </template>
