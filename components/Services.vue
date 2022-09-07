b1ef7f62922374b34f1271c5a7d5f86716a3db5e
Salaou-Deen Paraïso <pariso03henri@gmail.com>
2022-09-07 12:00:29 +0100

    add service component

---
+++ components/Services.vue
@@ -0,0 +1,155 @@
+<template>
+  <section class="about-section container">
+    <div class="hero" style="">
+      <div class="hero-body about-hero">
+        <div class="title is-size-4 has-text-centered">
+          <h1 class="has-text-weight-bold has-text-primary">
+            WHAT WE DO ?
+            <!-- <span></span>
+            <b-icon
+              style="color: #2BABE2"
+              icon="chat-alert-outline"
+              size="is-small"
+            >
+            </b-icon> -->
+          </h1>
+          <h1 class="has-text-weight-bold has-text-black mt-5 ">
+            <strong>FEEL FREE TO DISCOVER OUR SERVICES</strong>
+          </h1>
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="box">
+              <div class="konsole-icon" style="margin-bottom:15px"></div>
+              <div>
+                <h4
+                  class="
+                    title
+                    is-size-5
+                    has-text-weight-bold has-text-left
+                   
+                  "
+                  style="margin-bottom:15px"
+                >
+                  Development
+                </h4>
+              </div>
+              <div style="margin-bottom:15px">
+                <h4>Scalable web and apps implementation.</h4>
+              </div>
+              <div style="display: flex; justify-content: flex-end">
+                <button class="button is-primary is-outlined">See Details >></button>
+              </div>
+            </div>
+          </div>
+          <div class="column"> 
+             <div class="box">
+              <div class="paint-icon" style="margin-bottom:15px"></div>
+              <div>
+                <h4
+                  class="
+                    title
+                    is-size-5
+                    has-text-weight-bold has-text-left
+                   
+                  "
+                  style="margin-bottom:15px"
+                >
+                  Design
+                </h4>
+              </div>
+              <div style="margin-bottom:15px">
+                <h4>User Interface and User Experience.</h4>
+              </div>
+              <div style="display: flex; justify-content: flex-end">
+                <button class="button is-primary is-outlined">See Details >></button>
+              </div>
+            </div>
+          </div>
+          <div class="column">
+             <div class="box">
+              <div class="file-icon" style="margin-bottom:15px"></div>
+              <div>
+                <h4
+                  class="
+                    title
+                    is-size-5
+                    has-text-weight-bold has-text-left
+                   
+                  "
+                  style="margin-bottom:15px"
+                >
+                  Content Production
+                </h4>
+              </div>
+              <div style="margin-bottom:15px">
+                <h4>Written, audio and visual content.</h4>
+              </div>
+              <div style="display: flex; justify-content: flex-end">
+                <button class="button is-primary is-outlined">See Details >></button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rounded: true,
+    };
+  },
+};
+</script>
+
+
+<style>
+.tabs li.is-active a {
+  color: #2babe2;
+}
+.konsole-icon {
+  height: 100px;
+  width: 100px;
+  background-image: url("~/assets/images/konsole.png");
+}
+.paint-icon {
+  height: 100px;
+  width: 100px;
+  background-image: url("~/assets/images/palette.png");
+}
+.file-icon {
+  height: 100px;
+  width: 100px;
+  background-image: url("~/assets/images/file.png");
+}
+.center {
+  margin: 0;
+
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  -ms-transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%);
+}
+.about-text {
+  padding: 30px;
+}
+.about-img {
+  width: 150px;
+}
+
+.b-image-wrapper {
+  border: solid 2px;
+  border-radius: 100px;
+  margin-bottom: 10px;
+}
+@media screen and (max-width: 768px) {
+  .about-img {
+    margin: auto;
+  }
+} ;
+</style>
