94e778449937e2a9eb3a800ea9a54b1dd8e3cba9
Salaou-Deen Paraïso <pariso03henri@gmail.com>
2022-09-11 22:46:38 +0100

    -  update main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,27 @@
 <template>
   <section class="section">
-
-        <div class="columns">
-          <div class="column has-text-centered hero1">
-            <img src="~/assets/images/pannel.gif" alt="">
-            <h2 class="title is-2 has-text-primary">We Are Under Maintenance</h2>
-            <p class="subtitle is-4">We will be back soon !</p>
-          </div>
+    <div class=" container1">
+      <div class=" vertical-center has-text-centered ">
+        <img src="~/assets/images/pannel.gif" alt="" />
+        <div>
+          <img
+            width="90px"
+            src="~/assets/henri-dev-circle.png"
+            alt="{Henri-dev}"
+          />
         </div>
-
+      <h2 class="title is-2 has-text-primary">We Are Under Maintenance</h2>
+        <p class="subtitle has-text-weight-semibold has-text-black is-4">
+          We will be back soon !
+        </p>
+      </div>
+    </div>
   </section>
 </template>
 
 
 <script>
-
 export default {
-  
   name: "Home",
   data() {
     return {
@@ -29,22 +34,23 @@ export default {
 </script>
 
 <style>
-.hero-img {
-  background-repeat: no-repeat;
-  background-size: cover;
-  background-position: center;
-}
-.hero1 {
+
+/* .hero1 {
   max-width: fit-content;
   margin: auto;
-}
-.center {
+} */
+.vertical-center {
+  width: fit-content;
   margin: 0;
-
   position: absolute;
   top: 50%;
   left: 50%;
   -ms-transform: translate(-50%, -50%);
   transform: translate(-50%, -50%);
+}
+.container1 {
+  height: 500px;
+  position: relative;
+
 }
 </style>
