48acba98a00f7b39c8134aaeb76087e8c60fbc00
Salaou-Deen Paraïso <pariso03henri@gmail.com>
2022-12-20 15:59:33 +0100

    - update contact for company purpose

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,54 +1,49 @@
 <template>
   <div class="hero-body work-hero">
-    <div
-      class="has-text-centered"
-      style="margin-top: 15px; margin-bottom: 30px"
-    >
-      <p class="title is-size-4 is-size-5-mobile" style="padding-bottom: ">
-        Contact
-      </p>
-    </div>
+    <div class="title is-size-4 has-text-centered mb-6">
+          <h1 class="has-text-weight-bold has-text-primary">
+           CONTACT US
+            <!-- <span></span>
+            <b-icon
+              style="color: #2BABE2"
+              icon="chat-alert-outline"
+              size="is-small"
+            >
+            </b-icon> -->
+          </h1>
+          <h1 class="has-text-weight-bold  mt-5 has-text-link ">
+            <strong>WE’D LOVE TO HEAR FROM YOU !</strong>
+          </h1>
+        </div>
     <div class="columns">
-      <div class="column is-half">
-        <div class="contact-img"></div>
-      </div>
-
-      <div class="column">
+      <div class="column is-half m-auto">
+        
         <form action="">
-          <b-field label="Nom">
+          <b-field label="Nom"
+          type="is-link"
+          >
+            
             <b-input></b-input>
           </b-field>
-          <b-field label="Email">
+          <b-field label="Email" type="is-link">
             <b-input type="email"></b-input>
           </b-field>
-          <b-field label="Message">
+          <b-field label="Message" type="is-link">
             <b-input maxlength="200" type="textarea"></b-input>
           </b-field>
         </form>
         <b-button type="is-primary" expanded outlined> ENVOYER </b-button>
       </div>
     </div>
+     <hr style=" border-top: 1px solid #BB9140;">
   </div>
 </template>
 
 
 
 <style>
-.contact-img {
-  background-repeat: no-repeat;
-  padding: 20px;
-  background-size: contain;
-  background-position: center;
-  background-image: url("../assets/images/4.png");
-  height: 300px;
-  margin: 0;
-}
 
-@media screen and (min-width: 768px) {
-  .contact-img {
-    margin-top: 60px;
-  }
-}
+
 
 .center {
   margin: 0;
