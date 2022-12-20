d03985810e0d69ebbfee79012be550e4a76d62be
Salaou-Deen Paraïso <pariso03henri@gmail.com>
2022-12-20 16:00:39 +0100

    - update footer  for company purpose

--- layouts/partials/Footer.vue
+++ layouts/partials/Footer.vue
@@ -1,27 +1,15 @@
 <template>
   <footer class="footer">
-    <div class="content has-text-centered">
+    <div class="content has-text-centered has-text-primary has-text-weight-bold">
       <p>
-        <strong>Henri-dev </strong> par
-        <a href="https://henri-dev.com">Sadi Henri Paraïso</a>. <br />
-        Développé avec <a href="https://nuxtjs.org">Nuxtjs </a> +
-        <a href="https://buefy.org">Buefy </a> +
-        <a href="https://www.djangoproject.com/m">Django</a>. <br />
-        <a href="https://github.com/henrid3v/henri-front">Code source </a>
-        déposé sous la licence
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. <br />
-        Le contenu de ce site est déposé sous la licence
-        <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/"
-          >CC BY NC SA 4.0</a
-        >
-        <br />
+      
+    Copyright © Kreativ Inc 2022
+ 
       </p>
     </div>
   </footer>
 </template>
 
 <style>
-  a{
-    color:#2BABE2;
-  }
+ 
 </style>
