9062a9d26b3b997894a6cdc9c22e3782ecaade2e
Salaou-Deen Paraïso <pariso03henri@gmail.com>
2022-12-20 16:10:29 +0100

    - update header info

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Henri-dev',
+    title: 'Kreativ-INC',
     htmlAttrs: {
       lang: 'fr'
     },
